b4b0cbc145c4d9d367702d524ab18ec6720087da
josh okello <joshua.okello@hotmail.com>
2016-03-01 19:59:30 +0300

    changes with phpdotenv

--- WEB/PHP/dashboard/db.php
+++ WEB/PHP/dashboard/db.php
@@ -1,9 +1,17 @@
 <?php
-$connection = mysql_connect('localhost', 'safepalAdm1n', 'Af3ceybTA9Yt6nszHvkCetq');
+
+require_once '../vendor/autoload.php'; //autoload all app namespaces/classes
+
+//dotenv
+$dotenv = new Dotenv\Dotenv('../', ".env.php");
+$dotenv->load();
+
+
+$connection = mysql_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PWD'));
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('SFPdb');
+$select_db = mysql_select_db(getenv('DB_NAME'));
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }
