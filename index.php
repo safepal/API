9a83559f2ca3af337b6134ef14c8f513adfb935b
Joshua Okello <Joshua Okello>
2017-02-23 16:16:16 +0300

    init8.5

--- index.php
+++ index.php
@@ -38,9 +38,29 @@ $app->get('/', function (Request $req, Response $res){
 	return $res;
 });
 
-$app->get('/test', function (Request $req, Response $res){
-	$res->getBody()->write("SafePal API v1.");
-	return $res;
+$app->post('/test', function (Request $req, Response $res){
+
+	if (empty($req->getParsedBody())) {
+		throw new InvalidArgumentException("Empty request");
+	}
+
+	$data = json_decode($req->getParsedBody());
+
+	$pd = new PDO("mysql:host=".getenv('HOST').";dbname=".getenv('DB').",".getenv('DBUSER').",".getenv('DBPWD'));
+	$pd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	$pd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	$pd->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+	$nCSO = $pd->prepare("INSERT INTO Apitest VALUES (?)")->execute($data['name']);
+	//$pd->query($nCSO);
+	//$result = $pd->execute(filter_var_array($nCSO));
+
+	if ($nCSO) {
+		$res->withJson(json_encode($nCSO));
+	}
+	
+	$pd = null;
 });
 
 //run app
